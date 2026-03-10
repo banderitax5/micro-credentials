f02dc465769057665d43cfb619869450142ada19
banderitax5 <r2023025971@feuroosevelt.edu.ph>
2026-03-10 12:10:29 +0800

    adding navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+import Home from './pages/Home.jsx'
 
 function App() {
 
   return (
     <>
-      asdf
-      asdf
-      
+      <Navbar />
+      <Home />
+      <Footer />
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+
+    return (
+        <body className="bg-white w-screen h-screen">
+            
+        </body>
+    )
+
+}
+
+export default Home
